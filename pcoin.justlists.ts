43be9146f0ad802ed854a599e8ac83e414db75a1
PCoCrypto <108300866+pumacoincrypto@users.noreply.github.com>
2022-06-28 00:44:20 +0430

    Create pcoin.justlists.ts

---
+++ pcoin.justlists.ts
@@ -0,0 +1,20 @@
+{
+        "name": "Pcoin",
+        "logoURI": "https://www.pcocrypto.com/images/logo2x.png",
+        "timestamp": 1645516281000,
+        "tokens": [
+               {
+                     "chainId": 1,
+                     "address": "TYnBXH9TsuzY8rviUN5EKytKf7D99z3gci",
+                     "name": "Puma Coin",
+                     "symbol": "PCOIN",
+                     "decimals": 6,
+                     "logoURI": "https://www.pcocrypto.com/images/logo2x.png"
+               }
+         ],
+         "version": {
+                "major": 1,
+                "minor": 0,
+                "patch": 0
+        }
+}
